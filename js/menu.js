4ddc3f12fe7e48782392842cbb92722fb29f15ad
PedroBoscachi <pedroboscachi@gmail.com>
2022-05-30 15:09:38 -0300

    testanto animações

--- js/menu.js
+++ js/menu.js
@@ -3,6 +3,7 @@ const btn = document.getElementById('btn');
 function toggleMenu() {
     const nav = document.getElementById('nav');
     nav.classList.toggle('active');
+    nav.classList.add("menuLink");
 }
 
 btn.addEventListener('click', toggleMenu);
